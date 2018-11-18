15dff5e504ea8ee14bc8b3c515d7b3be7c42f690
Jenny ZHANG <zhangshunhang123@gmail.com>
2018-11-18 23:03:13 +0800

    regulation & syllabus page finish

---
+++ CS_Homepage/Curriculum/RegulationsSyllabusViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegulationsSyllabusViewController.swift
+//  CS_Homepage
+//
+//  Created by Jenny ZHANG on 2018/11/18.
+//  Copyright © 2018年 Jenny ZHANG. All rights reserved.
+//
+
+import UIKit
+
+class RegulationsSyllabusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
